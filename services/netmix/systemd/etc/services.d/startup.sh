2c8702a2b792c6211fdf591634b49263342269f6
yangteng <5148338@qq.com>
2021-08-25 13:33:55 +0800

    增加netmix容器

--- services/netmix/systemd/etc/services.d/startup.sh
+++ services/netmix/systemd/etc/services.d/startup.sh
@@ -4,15 +4,19 @@
 OPENVPN_CERT_CONF='/etc/openvpn/client.conf'
 TINYPROXY_CONF='/etc/tinyproxy/tinyproxy.conf'
 
+# for openvpn
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+chmod -R 777 /etc/openvpn
+
 # 有openvpn证书时连接vpn
-if [[ -f $OPENVPN_CERT_CONF ]]
-    then
-      mkdir -p /dev/net
-      mknod /dev/net/tun c 10 200
-      chmod 600 /dev/net/tun
-      /usr/sbin/openvpn --config $OPENVPN_CERT_CONF
+if [ -f $OPENVPN_CERT_CONF ]; then
+  /usr/sbin/openvpn --user nobody --group nobody --daemon --config $OPENVPN_CERT_CONF
 fi
+
 # 启动tinyproxy
 /usr/local/bin/tinyproxy -c $TINYPROXY_CONF
+
 # 启动ssh
 /usr/sbin/sshd -D
